36b3e9434d6b61569dbd036c3de80fe49079714f
Manon <m.ventura@vatel.fr>
2022-10-06 17:55:40 +0200

    modalup.js presque fini

--- components/modals/modalup.js
+++ components/modals/modalup.js
@@ -1,84 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import styles from "../../styles/modal.module.css";
+import Link from 'next/link'
 
 const Modal = ({ isShowing, hide }) =>
   isShowing
     ? ReactDOM.createPortal(
         <>
-          <div className="modal-overlay">
-            <div className="modal-wrapper">
-              <div className="modal">
-                <div className="modal-header">
-                  <img classname="logo" src='logo3.png'/>
-                  <h4>Modal Header</h4>
+          <div className={styles.modalOverlay}>
+            <div className={styles.modalWrapper}>
+              <div className={styles.modal}>
+                  <img classname={styles.logo} style={{width:'10%', borderRadius:'50%'}} src='amin2.png'/>
+                <div className={styles.modalHeader}>
+                  <h4 className={styles.modalTitle} >Create your Aminator account</h4>
                   <button
                     type="button"
-                    className="modal-close-button"
+                    className={styles.modalCloseButton}
                     onClick={hide}
                   >
                     <span>&times;</span>
                   </button>
                 </div>
-                <div className="modal-body">Hello Modal Here</div>
+                <div className={styles.modalBody}>
+                  <input className={styles.input} placeholder='Name'/>
+                  <input className={styles.input} placeholder='Username' />
+                  <input className={styles.input} placeholder='Password'/>
+                  <button className={styles.buttonSignUp}>Signup</button>
+                </div>
               </div>
             </div>
           </div>
-
-          <style jsx="true">{`
-            .modal-overlay {
-              position: fixed;
-              top: 0;
-              left: 0;
-              width: 100vw;
-              height: 100vh;
-              z-index: 1040;
-              background-color: rgba(0, 0, 0, 0.5);
-            }
-            .logo {
-              height: 10px;
-              width: 10px
-            }
-            .modal-wrapper {
-              position: fixed;
-              top: 0;
-              left: 0;
-              z-index: 1050;
-              width: 100%;
-              height: 100%;
-              overflow-x: hidden;
-              overflow-y: auto;
-              outline: 0;
-              display: flex;
-              align-items: center;
-            }
-
-            .modal {
-              z-index: 100;
-              background: #fff;
-              position: relative;
-              margin: auto;
-              border-radius: 5px;
-              max-width: 500px;
-              width: 80%;
-              padding: 1rem;
-            }
-
-            .modal-header {
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-            }
-
-            .modal-close-button {
-              font-size: 1.4rem;
-              font-weight: 700;
-              color: #000;
-              cursor: pointer;
-              border: none;
-              background: transparent;
-            }
-          `}</style>
+   
         </>,
         document.body
       )
